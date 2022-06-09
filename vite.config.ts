a737d96e4eec2ed5239064fa1abce3c696e24d53
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-06-09 09:15:30 +0900

    apple-iconを設定する Fixed #4

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.json';
 
 export default defineConfig({
-  plugins: [solidPlugin(), tsconfigPaths(), VitePWA({})],
+  plugins: [
+    solidPlugin(),
+    tsconfigPaths(),
+    VitePWA({
+      manifest: manifest,
+      includeAssets: [
+        'robots.txt',
+        'icons/*.{png,ico}',
+      ],
+    }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
